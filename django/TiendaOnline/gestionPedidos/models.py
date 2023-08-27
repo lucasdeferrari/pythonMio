84540aa5cec3c720fd7098ac8f624d2dc8a187c8
lucasDeferrari <ldeferrari@frba.utn.edu.ar>
2023-08-27 12:55:54 -0300

    postresql y panel de admin

--- django/TiendaOnline/gestionPedidos/models.py
+++ django/TiendaOnline/gestionPedidos/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Clientes(models.Model):
     nombre=models.CharField(max_length=30)
     direccion=models.CharField(max_length=50)
-    email=models.EmailField()
+    # AGREGO ESOS CAMPOS PARA PERMITIR QUE EL MAIL NO SEA OBLIGATORIO
+    email=models.EmailField(blank=True,null=True)
     telefono=models.CharField(max_length=7)
 
 class Articulos(models.Model):
@@ -13,10 +14,11 @@ class Articulos(models.Model):
     seccion=models.CharField(max_length=20)
     precio=models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre es %s la seccion es %s y el precio es %s' %(self.nombre, self.seccion,self.precio)
+
 class Pedidos(models.Model):
     numero=models.IntegerField()
     fecha=models.DateField()
     entregado=models.BooleanField()
-
-#sdjfnds
 
